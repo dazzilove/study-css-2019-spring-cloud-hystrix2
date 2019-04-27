d04fc414d08f3cecc63de792f8d3a4dfc96ba478
10028467 <dazzilove@sk.com>
2019-04-27 15:19:21 +0900

    turbine 관련 코드 추가

--- hystrixdashboard/src/main/java/com/example/hystrixdashboard/HystrixdashboardApplication.java
+++ hystrixdashboard/src/main/java/com/example/hystrixdashboard/HystrixdashboardApplication.java
@@ -3,9 +3,11 @@ package com.example.hystrixdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class HystrixdashboardApplication {
 
 	public static void main(String[] args) {
